bbb925a359dac5dc4051cfbc8b2983db5f998007
shrenik879 <shrenikkondekar@gmail.com>
2026-01-19 17:36:27 +0530

    Fix login cookie for production

--- controllers/user.js
+++ controllers/user.js
@@ -56,11 +56,13 @@ async function handleUserLogin(req, res) {
   }
 
   const token = setUser(user);
+  
   res.cookie("token", token, {
-    httpOnly: true,
-    sameSite: "lax",
-  });
-
+  httpOnly: true,
+  secure: true,        // REQUIRED on Render (HTTPS)
+  sameSite: "none",    // REQUIRED for Vercel ↔ Render
+  maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+});
   return res.json({
     message: "Login successful",
     user: {
