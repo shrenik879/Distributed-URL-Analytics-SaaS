e31e476a35ac9c96ba3df2567063e2c696eb5401
shrenik879 <shrenikkondekar@gmail.com>
2026-02-26 23:49:13 +0530

    debug env check

--- admin-panel/src/api.js
+++ admin-panel/src/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
+console.log("ENV VALUE:", import.meta.env.VITE_BACKEND_URL);
 const api = axios.create({
+    
     baseURL: import.meta.env.VITE_BACKEND_URL,
     withCredentials: true,
 });
